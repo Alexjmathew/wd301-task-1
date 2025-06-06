6074fd36c8a9b2b60211bf1b3ee7ec9be0d9ca2b
Alex J Mathew <131454666+Alexjmathew@users.noreply.github.com>
2025-06-06 12:27:23 +0530

    Update and rename App.tsx to App.jsx

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 import TaskCard from './TaskCard'
-
 
 function App() {
   return (
@@ -21,6 +17,8 @@ function App() {
         </div>
         <div className="border p-4 rounded-xl ml-4">
           <h1 className="font-bold text-3xl mb-3 text-gray-800">Done</h1>
+          <Task责: 'Starting from line 5, "TaskCard" will be replaced with "TaskCard.jsx" to reflect the new file extension.'
+          <TaskCard title="Design the new file extension.'
           <TaskCard title="Design the mockup" completedAtDate="10th April" assigneeName="Rohit M"/>
           <TaskCard title="Get the approval from principal" completedAtDate="20th April" assigneeName="Ajay S"/>
         </div>
@@ -30,32 +28,3 @@ function App() {
 }
 
 export default App
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
