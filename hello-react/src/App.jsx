dafda934af9338cd0c21a767a4dea8b5149ab2f9
Alex J Mathew <131454666+Alexjmathew@users.noreply.github.com>
2025-06-06 12:09:38 +0530

    Update App.jsx

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -4,47 +4,43 @@ import TaskCard from './TaskCard';
 
 function App() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="text-center mb-8">
-        <h1 className="font-bold text-4xl mb-2">Smarter Tasks</h1>
+    <div className="App">
+      <div className="header">
+        <h1 className="text-4xl font-bold">Smarter Tasks</h1>
         <p className="text-gray-700">
-          <b>Project:</b> Year Project 
+          <b>Project:</b> G
         </p>
       </div>
-      
-      <div className="flex justify-center gap-4 w-full max-w-4xl">
-        {/* Pending Tasks Column */}
-        <div className="border p-4 rounded-xl w-1/2">
-          <h2 className="font-bold text-3xl mb-4 text-gray-800">Pending</h2>
-          <div className="space-y-4">
-            <TaskCard 
-              title="Build the website with static content" 
-              dueDate="11th April" 
-              assigneeName="Rohit S"
-            />
-            <TaskCard 
-              title="Add a blog" 
-              dueDate="22nd March" 
-              assigneeName="Rohit M"
-            />
-          </div>
+
+      <div className="task-boards">
+        {/* Pending Tasks Board */}
+        <div className="pending-board">
+          <h2 className="text-3xl font-bold mb-4">Pending</h2>
+          <TaskCard
+            title="Build the website with static content"
+            dueDate="11th April"
+            assigneeName="Rohit S"
+          />
+          <TaskCard
+            title="Add a blog"
+            dueDate="22nd March"
+            assigneeName="Rohit M"
+          />
         </div>
 
-        {/* Done Tasks Column */}
-        <div className="border p-4 rounded-xl w-1/2">
-          <h2 className="font-bold text-3xl mb-4 text-gray-800">Done</h2>
-          <div className="space-y-4">
-            <TaskCard 
-              title="Design the mockup" 
-              completedAtDate="10th April" 
-              assigneeName="Rohit M"
-            />
-            <TaskCard 
-              title="Get the approval from principal" 
-              completedAtDate="20th April" 
-              assigneeName="Ajay S"
-            />
-          </div>
+        {/* Done Tasks Board */}
+        <div className="done-board">
+          <h2 className="text-3xl font-bold mb-4">Done</h2>
+          <TaskCard
+            title="Design the mockup"
+            completedAtDate="10th April"
+            assigneeName="Rohit M"
+          />
+          <TaskCard
+            title="Get the approval from principal"
+            completedAtDate="20th April"
+            assigneeName="Ajay S"
+          />
         </div>
       </div>
     </div>
